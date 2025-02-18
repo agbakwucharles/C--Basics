ba93c779a4cf9c7b44f1f7e798f3576efdb56f6c
Charles <agbakwucharles@gmail.com>
2025-02-18 12:56:45 -0600

    Add files via upload

---
+++ Advanced Features/Program.cs
@@ -0,0 +1,92 @@
+﻿// Delegates and Events
+using System;
+
+public delegate void Notify();  // Delegate
+
+public class ProcessBusinessLogic
+{
+    public event Notify ProcessCompleted; // Event
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        // Some process logic here
+        OnProcessCompleted();
+    }
+
+    protected virtual void OnProcessCompleted()
+    {
+        ProcessCompleted?.Invoke();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += Bl_ProcessCompleted; // Subscribe to event
+        bl.StartProcess();
+    }
+
+    private static void Bl_ProcessCompleted()
+    {
+        Console.WriteLine("Process Completed!");
+    }
+}
+
+// Lambda Expressions
+using System;
+using System.Collections.Generic;
+
+class LambdaExpressions
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> evenNumbers = numbers.FindAll(x => x % 2 == 0); // Lambda expression
+
+        evenNumbers.ForEach(x => Console.WriteLine(x)); // Lambda expression
+    }
+}
+
+// LINQ
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        var evenNumbers = from num in numbers
+                          where num % 2 == 0
+                          select num;
+
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
+// Asynchronous Programming(async/await/Task)
+using System;
+using System.Threading.Tasks;
+
+class AsynchronousProgram
+{
+    static async Task Main(string[] args)
+    {
+        await DoWorkAsync();
+    }
+
+    static async Task DoWorkAsync()
+    {
+        Console.WriteLine("Starting work...");
+        await Task.Delay(2000); // Simulate async work
+        Console.WriteLine("Work completed!");
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BasicClassExample
+{
+    // Define a simple class named 'Person'
+    public class Person
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public static int Count = 0;
+
+        // Constructor
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Count++;
+        }
+
+        // Method to display person's details
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}, Age: {Age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create objects of the Person class
+            Person person1 = new Person("John", "Doe", 30);
+            person1.DisplayPersonInfo();
+            Console.WriteLine($"Total number of persons: {Person.Count}");
+
+            Person person2 = new Person("Jane", "Smith", 25);
+            person2.DisplayPersonInfo();
+            Console.WriteLine($"Total number of persons: {Person.Count}");
+        }
+    }
+}
